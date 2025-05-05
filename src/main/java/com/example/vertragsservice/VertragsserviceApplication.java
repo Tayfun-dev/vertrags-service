8e167847cd7066de3a10970935c8d566312bd4ef
Tayfun-dev <tgoemuekpinar@gmail.com>
2025-05-05 20:06:59 +0200

    Create VertragsserviceApplication.java

---
+++ src/main/java/com/example/vertragsservice/VertragsserviceApplication.java
@@ -0,0 +1,12 @@
+package com.example.vertragsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertragsserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VertragsserviceApplication.class, args);
+    }
+}
